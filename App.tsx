6399cb11ac139bd03e8bf8b40b30901e4eac776c
bruno918 <bruno-leal@educar.rs.gov.br>
2022-07-02 21:05:18 -0300

    aula1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button } from 'react-native-paper'
+
+function mudacor() {
+ const cores = ['green', 'dark', 'white', 'blue', 'green', 'red']
+ const alecor = cores[Math.floor(Math.random() * cores.length)]
+//  alecores = Math.floor(Math.random() * 13) + 349
+  document.body.style.backgroundColor = `${alecor}`;
+console.log(alecor)
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bom dia</Text>
       <StatusBar style="auto" />
+       <button onClick={mudacor}>
+    Press me
+  </button>
     </View>
   );
 }
@@ -13,7 +26,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
